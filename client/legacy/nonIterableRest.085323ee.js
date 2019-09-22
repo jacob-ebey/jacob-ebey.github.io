8a3adcfc0e1880de827514e5fc34031e62ca339e
Jacob Ebey <jacob.ebey@live.com>
2019-09-22 05:05:38 -0700

    Updates

---
+++ client/legacy/nonIterableRest.085323ee.js
@@ -0,0 +1,1 @@
+function r(r){if(Array.isArray(r))return r}function t(){throw new TypeError("Invalid attempt to destructure non-iterable instance")}export{r as _,t as a};
---
+++ client/legacy/toArray.ca118e71.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index.b40193dd.js";import{_ as r,a as o}from"./nonIterableRest.085323ee.js";function s(s){return r(s)||e(s)||o()}export{s as _};
